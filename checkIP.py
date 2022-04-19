90fdb8b67fc45a3bbc891fb1a042cc7346d263ff
Flickza <Adelholmg@gmail.com>
2022-04-19 12:44:43 +0200

    Added spacing for code readability.

--- checkIP.py
+++ checkIP.py
@@ -37,13 +37,16 @@ Subject: %s
 
 ## Getting the hostname of the device using socket.gethostname() method # Test commit
 hostname = socket.gethostname()
+
 ## getting the IP address using socket.gethostbyname() method
 ip_address = socket.gethostbyname(hostname)
 ipv4 = socket.gethostbyname(socket.gethostname())
+
 ## Gets ip address of raspberry pi device
 ipAdresse = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1],
  [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close())
 for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+
 ## printing the hostname and ip_address
 print(f"Hostname: {hostname}")
 print(f"IP Address: {ip_address}")
