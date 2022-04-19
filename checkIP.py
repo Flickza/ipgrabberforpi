f710ffa652a2ab3fe22d068a206450d946ab8e09
Flickza <Adelholmg@gmail.com>
2022-04-19 12:42:27 +0200

    Adding descriptive comments.

--- checkIP.py
+++ checkIP.py
@@ -40,6 +40,7 @@ hostname = socket.gethostname()
 ## getting the IP address using socket.gethostbyname() method
 ip_address = socket.gethostbyname(hostname)
 ipv4 = socket.gethostbyname(socket.gethostname())
+## Gets ip address of raspberry pi device
 ipAdresse = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1],
  [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close())
 for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
